62f12e7fb0ee9d9c46afd5b3e66799da6d47d38f
ACG <acg@MacBook-Air-de-ACG.local>
2023-08-29 21:33:11 -0300

    vo tests improvement

--- src/@seedwork/domain/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -5,16 +5,21 @@ import { validate as uuidValidate } from "uuid";
 // Aqui tem testes de integração, pois não faz sentido criar mais um arquivo
 // Pois se torna burocrático
 describe("UniqueEntityId Unit Tests", () => {
+  // Não é necessário, pois está habilitado como true no jest.config.ts
+  // beforeEach(() => {
+  //   jest.clearAllMocks();
+  // });
+
+  const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
+
   it("should throw error when uuid is invalid", () => {
     // as any pois validate é um método privado - ts reclama
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
     expect(() => new UniqueEntityId("fake id")).toThrow(new InvalidUuidError());
     // garante que o método validate está sendo chamado
     expect(validateSpy).toHaveBeenCalled();
   });
 
   it("should accept an uuid passed in constructor", () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
     const uuid = "2a5beb3b-ecd7-44e2-9d59-237a352b7d85";
     const vo = new UniqueEntityId(uuid);
     expect(vo.id).toBe(uuid);
@@ -22,7 +27,6 @@ describe("UniqueEntityId Unit Tests", () => {
   });
 
   it("should accept an uuid passed in constructor", () => {
-    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, "validate");
     const vo = new UniqueEntityId();
     expect(uuidValidate(vo.id)).toBeTruthy;
     expect(validateSpy).toHaveBeenCalled();
