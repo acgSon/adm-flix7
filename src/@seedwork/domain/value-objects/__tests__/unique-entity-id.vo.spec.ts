dbeecec3956d48f0a29c09d5e9c160f72e0ca6b4
acgSon <acgp7@icloud.com>
2023-09-02 20:48:23 +0000

    value object abstract class

--- src/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
+++ src/@seedwork/domain/value-objects/__tests__/unique-entity-id.vo.spec.ts
@@ -1,5 +1,5 @@
-import InvalidUuidError from "../errors/invalid-uuid.error";
-import UniqueEntityId from "./unique-entity-id.vo";
+import InvalidUuidError from "../../../errors/invalid-uuid.error";
+import UniqueEntityId from "../unique-entity-id.vo";
 import { validate as uuidValidate } from "uuid";
 
 // Aqui tem testes de integração, pois não faz sentido criar mais um arquivo
@@ -22,13 +22,13 @@ describe("UniqueEntityId Unit Tests", () => {
   it("should accept an uuid passed in constructor", () => {
     const uuid = "2a5beb3b-ecd7-44e2-9d59-237a352b7d85";
     const vo = new UniqueEntityId(uuid);
-    expect(vo.id).toBe(uuid);
+    expect(vo.value).toBe(uuid);
     expect(validateSpy).toHaveBeenCalled();
   });
 
   it("should accept an uuid passed in constructor", () => {
     const vo = new UniqueEntityId();
-    expect(uuidValidate(vo.id)).toBeTruthy;
+    expect(uuidValidate(vo.value)).toBeTruthy;
     expect(validateSpy).toHaveBeenCalled();
   });
 });
